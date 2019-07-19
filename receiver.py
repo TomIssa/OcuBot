b1668fb93e8692067bbb39df03ea0be7bb22befa
John <29266648+synchronocy@users.noreply.github.com>
2019-07-19 22:19:36 +1000

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,1 +1,18 @@
+import discord
+import datetime
 
+client = discord.Client()
+YOUR_TOKEN = ""
+@client.event
+async def on_ready():
+    print('Logged in as: '+str(client.user.name))
+    print('UserID: '+str(client.user.id))
+    print('User: '+str(client.user))
+    print('Email: '+str(client.user.email))
+    
+
+
+try:
+    client.run(YOUR_TOKEN)
+except Exception as e:
+    print(e)
